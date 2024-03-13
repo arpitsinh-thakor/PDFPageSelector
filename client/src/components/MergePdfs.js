fa721677c75c8d70e9f1b36c2622a8afacf638a9
arpitsinh-thakor <1803arpitthakor@gmail.com>
2024-03-13 17:29:45 +0530

    Merge PDFs from Client

--- client/src/components/MergePdfs.js
+++ client/src/components/MergePdfs.js
@@ -1,8 +1,34 @@
 import React from 'react'
 
 const MergePdfs = () => {
+  function handleSubmit(event){
+    event.preventDefault()
+    
+    const formData = new FormData(event.target)
+    fetch('http://localhost:4000/api/v1/mergePdfs', {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
   return (
-    <div>MergePdfs</div>
+    <div>
+      <h1>Merge PDFs</h1>
+      <form action="http://localhost:4000/api/v1/mergePdfs" 
+            method="post" encType="multipart/form-data"
+            onSubmit={handleSubmit}
+            >
+        <input type="file" name="file1"/>
+        <input type="file" name="file2"/>
+        <button type="submit">Merge</button>
+      </form>
+    </div>
   )
 }
 
